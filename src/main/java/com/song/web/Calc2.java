c7e0251a38ee03de63ee9b9ec7db5dcc7b315a3b
thdgus4025 <thdgus4025@gmail.com>
2023-11-03 18:07:41 +0900

    add calcpage

--- src/main/java/com/song/web/Calc2.java
+++ src/main/java/com/song/web/Calc2.java
@@ -15,9 +15,11 @@ import java.util.concurrent.ConcurrentHashMap;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/calc2")
 public class Calc2 extends HttpServlet {
@@ -26,6 +28,10 @@ public class Calc2 extends HttpServlet {
 			throws ServletException, IOException {
 //		빈문자열이 오는지도 체크해야함.
 		ServletContext application = request.getServletContext();
+		HttpSession session = request.getSession();
+		
+		Cookie[] cookies = request.getCookies();
+		
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
 
@@ -39,13 +45,35 @@ public class Calc2 extends HttpServlet {
 		// 계산
 		if(op.equals("=")) {
 			// application에 들어가있는 값을 꺼내옴
-			int x = (Integer)application.getAttribute("value");
+//			int x = (Integer)application.getAttribute("value");
+//			int x = (Integer)session.getAttribute("value");
+			
+			int x = 0;
+			for(Cookie c : cookies)
+				if(c.getName().equals("value")) {
+						x = Integer.parseInt(c.getValue());
+					break;
+				}
+			
+			
 			// 사용자가 입력한 값을 가져옴
 			int y = v;
 				
-			String operator = application.getAttribute("operator") == null ?
-					"" : application.getAttribute("operator").toString();
+//			String operator = application.getAttribute("operator") == null ?
+//					"" : application.getAttribute("operator").toString();
 			
+			String operator = "";
+			
+			for(Cookie c : cookies)
+				if(c.getName().equals("operator")) {
+						operator = c.getValue();
+					break;
+				}
+
+			
+//			String operator = session.getAttribute("operator") == null ?
+//					"" : session.getAttribute("operator").toString();
+//			
 			int result = 0;
 			
 			if(operator.equals("+"))
@@ -54,10 +82,23 @@ public class Calc2 extends HttpServlet {
 				result = x - y;
 			response.getWriter().printf("result is %d\n", result);
 		} else { // 값을 저장
-			application.setAttribute("value", v);
-			application.setAttribute("operator", op);			
+//			application.setAttribute("value", v);
+//			application.setAttribute("operator", op);
+			
+//			session.setAttribute("value", v);
+//			session.setAttribute("operator", op);
+			
+			Cookie valueCookie = new Cookie("value", String.valueOf(v));
+			Cookie OpCookie = new Cookie("operator", op);
+			valueCookie.setPath("/calc2"); // 모든페이지에 valueCookie를 가져옴
+			valueCookie.setMaxAge(24*60*60);
+			OpCookie.setPath("/calc2"); // 모든페이지에 valueCookie를 가져옴
+			response.addCookie(valueCookie);
+			response.addCookie(OpCookie);
+			
+			response.sendRedirect("calc2.html");
+			
 		}
-
 		
 	}
 }
---
+++ src/main/java/com/song/web/Calc3.java
@@ -0,0 +1,76 @@
+package com.song.web;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/calc3")
+public class Calc3 extends HttpServlet {
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+//		빈문자열이 오는지도 체크해야함.
+		ServletContext application = request.getServletContext();
+		HttpSession session = request.getSession();
+		
+		Cookie[] cookies = request.getCookies();
+		
+		String value = request.getParameter("value");
+		String operator = request.getParameter("operator");
+		String dot = request.getParameter("dot");
+			
+		String exp = "";
+		
+		if(cookies != null) {
+			for(Cookie c : cookies)
+				if(c.getName().equals("exp")) {
+					exp = c.getValue();
+					break;
+				}
+		
+		if(operator != null && operator.equals("=")) {
+			ScriptEngine engine = new ScriptEngineManager().getEngineByName("graal.js");
+					try {
+						exp = String.valueOf(engine.eval(exp));
+					} catch (ScriptException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+		}else if(operator != null && operator.equals("C")) {
+			exp = "";
+		}else {
+			exp += (value == null)? "" : value;
+			exp += (operator == null)? "" : operator;
+			exp += (dot == null)? "" : dot;
+		}
+		
+		Cookie expCookie = new Cookie("exp", exp );
+		if(operator != null && operator.equals("C")) {
+			expCookie.setMaxAge(0);
+		}
+		response.addCookie(expCookie);
+		response.sendRedirect("calcpage");
+		
+		}
+	}
+}
